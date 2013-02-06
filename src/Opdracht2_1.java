f4f766c33384e9f6a48d1641e22cb934809657e3
raymond <rayrohder@gmail.com>
2013-02-06 15:17:53 +0100

    2DGraphics improved

--- src/Opdracht2_1.java
+++ src/Opdracht2_1.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.nio.file.Path;
+
 import javax.swing.*;
 
 
@@ -54,18 +56,30 @@ public class Opdracht2_1 extends JFrame
 		
 		public void paintComponent(Graphics g) 
 		{
-		    Graphics2D g2 = (Graphics2D) g;
-		    int t = 7;
+			Graphics2D g2 = (Graphics2D) g;
+		    
+		    
 		    g2.translate(getWidth()/2, getHeight()/2);
 		    
-		    if(t < 8)
-		    {
+		    int x1 = 0;
+    		int y1 = 0;
+    		
+    		g2.drawLine(-300, -0, 300, 0);
+		    g2.drawLine(0, -300, 0, 300);
+		    
+		    for(double t = -8; t < 8; t++)
+		    {   	
+		    				
 		    	int x = (int)Math.pow(t, 2);
 		    	int y = (int)Math.pow(t, 3);
-		        g2.drawLine(0, 0, x , y);    
+		    	g2.drawLine(x1, y1, x, y);
+		    	
+		    	x1 = x;
+		    	y1 = y;
+		    	
+		    	//try{Thread.sleep(1000L);
+		    		//}catch(Exception e){}
 		    }
-		    else
-		    	System.out.println("kan niet");
 		}
 	}
 
